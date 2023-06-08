3c9cba8cf0dfbebd5d47a6a72b59072db4a53f27
Lalith Sai Thomala <thomalalalithsai@gmail.com>
2023-06-08 14:17:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,29 +17,4 @@ if uploaded_file is not None:
     st.subheader("Raw Data")
     st.dataframe(df)
 
-    # Generate bar chart
-    st.subheader("Data Visualization - Bar Chart")
-    st.write("Select the column for the x-axis:")
-    x_axis = st.selectbox("X-axis", df.columns)
-    st.write("Select the column for the y-axis:")
-    y_axis = st.selectbox("Y-axis", df.columns)
-    plt.bar(df[x_axis], df[y_axis])
-    plt.xlabel(x_axis)
-    plt.ylabel(y_axis)
-    st.pyplot(plt)
-
-    # Download the processed data
-    st.subheader("Download Processed Data")
-    csv_data = df.to_csv(index=False)
-    st.download_button(
-        label="Download CSV",
-        data=csv_data,
-        file_name="processed_data.csv",
-        mime="text/csv"
-    )
-
-    # Separate forecast for items
-    st.subheader("Item Forecast")
-    item_column = st.selectbox("Select the column for item names:", df.columns)
-    forecast_data = df.groupby(item_column).sum()
-    st.dataframe(forecast_data)
+    
